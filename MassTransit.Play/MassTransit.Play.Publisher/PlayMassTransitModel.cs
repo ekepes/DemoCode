cad0f07c97f5eb74b09fd48a17ec4caaf619e1ff
Eric Kepes <eric.kepes@mckesson.com>
2011-03-01 16:09:39 -0500

    Missed files.

---
+++ MassTransit.Play/MassTransit.Play.Publisher/PlayMassTransitModel.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit.NinjectIntegration;
+using MassTransit.Transports;
+using MassTransit.Transports.Msmq;
+
+namespace MassTransit.Play.Publisher
+{
+    public class PlayMassTransitModel : MassTransitModuleBase
+    {
+        public override void Load()
+        {
+            base.Load();
+
+            RegisterEndpointFactory(x =>
+            {
+                x.RegisterTransport<LoopbackEndpoint>();
+                x.RegisterTransport<MulticastUdpEndpoint>();
+                x.RegisterTransport<MsmqEndpoint>();
+            });
+
+            RegisterServiceBus(@"msmq://localhost/mt_mike_publisher", x =>
+            {
+                x.SetConcurrentConsumerLimit(1);
+
+                ConfigureSubscriptionClient(@"msmq://localhost/mt_subscriptions", x);
+            });
+        }
+    }
+}

9bcce4ff4c99bb86eb19cf159abee7651a6be26b
ekepes <ekepes@gmail.com>
2013-08-18 19:48:28 -0400

    The shell of my TableStorage experiments.

---
+++ Azure/TableStorage/EventyStoreySitey/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace EventyStoreySitey.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private const string _value = "value";
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return _value;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

5582f07013e5b21c491d9c1429b3bfe1954793b6
ekepes <ekepes@gmail.com>
2013-12-15 15:11:50 -0500

    Greg Youngs Probability Kata

---
+++ ProbabilityKata/ProbabilityKata/Probability.cs
@@ -0,0 +1,51 @@
+﻿
+namespace ProbabilityKata
+{
+    public class Probability
+    {
+        private readonly decimal _probability;
+
+        public Probability(decimal probability)
+        {
+            _probability = probability;
+        }
+
+        public Probability CombinedWith(Probability probability)
+        {
+            return new Probability(_probability * probability._probability);
+        }
+
+        protected bool Equals(Probability other)
+        {
+            return _probability == other._probability;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Probability) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _probability.GetHashCode();
+        }
+
+        public Probability InverseOf()
+        {
+            return new Probability(1m - _probability);
+        }
+
+        public Probability Either(Probability probability)
+        {
+            return new Probability(_probability + probability._probability - CombinedWith(probability)._probability);
+        }
+
+        public static Probability operator +(Probability p1, Probability p2)
+        {
+            return new Probability(p1._probability + p2._probability);
+        }
+    }
+}
---
+++ ProbabilityKata/ProbabilityKataTests/ProbabilityTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using ProbabilityKata;
+
+namespace ProbabilityKataTests
+{
+    [TestFixture]
+    public class ProbabilityTests
+    {
+        [Test]
+        public void Half_combined_with_half_is_quarter()
+        {
+            var expected = new Probability(0.25m);
+            var actual = new Probability(0.5m).CombinedWith(new Probability(0.5m));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Inverse_of_quarter_is_three_quarters()
+        {
+            var expected = new Probability(0.75m);
+            var actual = new Probability(0.25m).InverseOf();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Either_hal_or_half_is_three_quarters()
+        {
+            var expected = new Probability(0.75m);
+            var actual = new Probability(0.5m).Either(new Probability(0.5m));
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

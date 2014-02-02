f17b9039b51ea6536ecb996d04a4cdd5b09d7c57
Eric Kepes <ekepes@gmail.com>
2014-02-02 10:13:17 -0500

    Just some stuff

--- erlang/drop.erl
+++ erlang/drop.erl
@@ -1,5 +1,7 @@
 -module(drop). 
--export([fall_velocity/2]).
+-export([fall_velocity/1]).
+
+fall_velocity({Planemo, Distance}) -> fall_velocity(Planemo, Distance).
 
 fall_velocity(earth, Distance) when Distance >= 0 -> math:sqrt(2 * 9.8 * Distance);
 

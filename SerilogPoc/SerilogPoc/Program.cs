414f36abae7c028d7534e5e7a97902b106848295
Kepes, Eric <eric.kepes@aesynt.com>
2016-03-15 11:41:39 -0400

    Experiments with learning Serilog.

---
+++ SerilogPoc/SerilogPoc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Serilog;
+
+namespace SerilogPoc
+{
+    // Good stuff here: http://blachniet.com/blog/serilog-good-habits/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFormat = "{Timestamp} [{Level}] ({ThreadId}) {SourceContext:l} {Message}{NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithThreadId()
+                .Enrich.WithMachineName()
+                .WriteTo.ColoredConsole(outputTemplate:
+        logFormat)
+                .WriteTo.RollingFile(@"C:\tmp\Log-{Date}.txt", outputTemplate:
+        logFormat)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            var log = Log.ForContext<Program>();
+
+            log.Information("Hello, Serilog!");
+
+            log.Debug("Debug");
+            log.Information("Info");
+            log.Warning("Warning");
+            log.Error("Error");
+
+            Console.ReadLine();
+        }
+    }
+}

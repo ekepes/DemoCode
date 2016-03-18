4820c38fbe8cf69572e1f7a3d049162d438c3300
Eric Kepes <eric@kepes.net>
2016-03-17 21:27:12 -0400

    First test, and it fails

---
+++ pythonset/mysettest.py
@@ -0,0 +1,10 @@
+import myset
+import unittest
+
+class SetTests(unittest.TestCase):
+  def test_a_new_set_isempty(self):
+    set = myset.Set()
+    self.assertEqual(set.isEmpty, True)
+
+if __name__ == '__main__':
+  unittest.main()

3e6d8c5edb2bae044efeaf2dee359eb0e3e70ab3
Eric Kepes <eric@kepes.net>
2016-03-19 12:19:32 -0400

    Failing test for clear

--- pythonset/mysettest.py
+++ pythonset/mysettest.py
@@ -16,11 +16,25 @@ class SetTests(unittest.TestCase):
     set.add("One")
     self.assertEqual(set.contains("One"), True)
 
+  def test_add_several_items_returns_correct_size(self):
+    set = Set()
+    set.add("One")
+    set.add("Two")
+    set.add("Three")
+    self.assertEqual(set.size(), 3)
+
   def test_add_item_twice_still_in_set_once(self):
     set = Set()
     set.add("One")
     set.add("One")
     self.assertEqual(set.size(), 1)
+
+  det test_clear_removes_all_items_from_the_set(self):
+    set = Set()
+    set.add("One")
+    set.add("Two")
+    set.clear()
+    self.assertEqual(set.size(), 0)
 
 if __name__ == '__main__':
   unittest.main()

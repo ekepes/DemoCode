c98c95cc55e412d5a45780873bb95a702893e974
Eric Kepes <eric@Erics-MacBook-Pro.local>
2018-10-22 11:03:41 -0400

    Figured out simple dotnetcore app on Mac

---
+++ dotnetcore-hello/app/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using library;
+
+// source: https://docs.microsoft.com/en-us/dotnet/core/tutorials/using-on-macos
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
---
+++ dotnetcore-hello/test-library/LibraryTests.cs
@@ -0,0 +1,13 @@
+using library;
+using Xunit;
+
+namespace test_library
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void TestThing() {
+            Assert.Equal(42, new Thing().Get(19, 23));
+        }
+    }
+}
